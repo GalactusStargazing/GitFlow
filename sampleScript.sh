7754bd577f7a742d718e596c8164b6b002a7b25b
Pranjal Yadav <109521568+GalactusStargazing@users.noreply.github.com>
2025-02-20 08:50:35 -0500

    Update sampleScript.sh
    Performing remote repository changes

--- sampleScript.sh
+++ sampleScript.sh
@@ -2,3 +2,6 @@ This is a sample file for testing!
 3rd commit for branch2
 2nd Commit for Branch2
 4th commit for branch2
+....................................
+Performing commit from remote repository
+....................................
