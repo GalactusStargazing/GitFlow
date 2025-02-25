ed562c1e3c88392ed3805c1e027a2071d423dbf2
Pranjal Yadav <109521568+GalactusStargazing@users.noreply.github.com>
2025-02-25 15:14:22 -0500

    Update date.sh

--- date.sh
+++ date.sh
@@ -1,4 +1,7 @@
 date
-echo "Successfully intigrated Jenkins with Git"
-pwd
+echo "1.Successfully intigrated Jenkins with Git"
+echo "2.List of files in git repo"
 ls
+echo "3.Below is current working directory for jenkins"
+pwd
+echo "cron job execution"
